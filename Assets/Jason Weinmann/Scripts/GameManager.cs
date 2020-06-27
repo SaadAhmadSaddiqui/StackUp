0e0886885bb80c8e258a620eb060dadfdeff5d0a
usmanarif24 <47153131+usmanarif24@users.noreply.github.com>
2020-06-28 03:29:28 +0400

    Cube Moving Functioning

---
+++ Assets/Jason Weinmann/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameManager : MonoBehaviour
+{
+      void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            MovingCube.CurrentCube.Stop();
+        }
+        
+    }
+}
---
+++ Assets/Jason Weinmann/Scripts/MovingCube.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour
+{
+    public static MovingCube CurrentCube { get; private set; }
+    public static MovingCube LastCube { get; private  set; }
+
+    [SerializeField]
+    private float MoveSpeed = 1f;
+
+
+    private void OnEnable()
+    {
+        if (LastCube == null)
+        {
+            LastCube = GameObject.Find("Start").GetComponent<MovingCube>();
+        }
+        CurrentCube = this;
+        
+    }
+
+
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * MoveSpeed;
+    }
+
+    internal void Stop()
+    {
+        MoveSpeed = 0;
+        float hangover = transform.position.z - LastCube.transform.position.z;
+        SplitCubeOnZ(hangover);
+        Debug.Log(hangover);
+    }
+
+    private void SplitCubeOnZ(float hangover)
+    {
+        float newZSize = LastCube.transform.localScale.z - Math.Abs(hangover);
+        float FallingBlockSize = transform.localScale.z - newZSize;
+        float NewZPosition = LastCube.transform.position.z + (hangover / 2);
+        transform.localScale = new Vector3( transform.localScale.x, transform.localScale.y, NewZPosition);
+        transform.position = new Vector3(transform.position.x, transform.position.y, NewZPosition);
+    }
+}
