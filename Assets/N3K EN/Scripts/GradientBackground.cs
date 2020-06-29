d3aa5ca3fc55ffa9aa588727968a569e975879f1
SaadAhmadSaddiqui <mehunsaadi@gmail.com>
2020-06-30 03:05:34 +0400

    Fixed the background color change and added gradient to it.

---
+++ Assets/N3K EN/Scripts/GradientBackground.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class GradientBackground : MonoBehaviour
+{
+
+    public Color topColor;
+    public Color bottomColor = Color.white;
+    public int gradientLayer = 7;
+    public Shader shader;
+    public float time;
+
+    private float H;
+    private float S;
+    private float V;
+    private Mesh mesh;
+    GameObject gradientPlane;
+    void Awake()
+    {
+        H = Random.Range(0.5f, 1f);
+        S = Random.Range(0.5f, 1f);
+        V = Random.Range(0.5f, 1f);
+        time = Random.Range(1f, 255f);
+        topColor = Random.ColorHSV();
+        gradientLayer = Mathf.Clamp(gradientLayer, 0, 31);
+
+        GetComponent<Camera>().clearFlags = CameraClearFlags.Depth;
+        GetComponent<Camera>().cullingMask = GetComponent<Camera>().cullingMask & ~(1 << gradientLayer);
+        Camera gradientCam = new GameObject("Gradient Cam", typeof(Camera)).GetComponent<Camera>();
+        gradientCam.depth = GetComponent<Camera>().depth - 1;
+        gradientCam.cullingMask = 1 << gradientLayer;
+
+        mesh = new Mesh();
+        mesh.vertices = new Vector3[4]
+        {new Vector3(-100f, .577f, 1f), new Vector3(100f, .577f, 1f), new Vector3(-100f, -.577f, 1f), new Vector3(100f, -.577f, 1f)};
+
+        mesh.colors = new Color[4] { topColor, topColor, bottomColor, bottomColor };
+
+        mesh.triangles = new int[6] { 0, 1, 2, 1, 3, 2 };
+
+        Material mat = new Material(shader);
+        gradientPlane = new GameObject("Gradient Plane", typeof(MeshFilter), typeof(MeshRenderer));
+
+        ((MeshFilter)gradientPlane.GetComponent(typeof(MeshFilter))).mesh = mesh;
+        gradientPlane.GetComponent<MeshRenderer>().material = mat;
+        gradientPlane.layer = gradientLayer;
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime * 2;
+        topColor = Color.HSVToRGB((time / 100f) % H, S, V);
+        mesh.colors = new Color[4] { topColor, topColor, bottomColor, bottomColor };
+        ((MeshFilter)gradientPlane.GetComponent(typeof(MeshFilter))).mesh = mesh;
+    }
+}
--- Assets/N3K EN/Scripts/RemoveRubble.cs
+++ Assets/N3K EN/Scripts/RemoveRubble.cs
@@ -5,30 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class RemoveRubble : MonoBehaviour
 {
-    public TheStack TheStack;
-
-    public float time;
-
-    private float H;
-    private float S;
-    private float V;
 
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(collision.gameObject);
     }
 
-    private void Start()
-    {
-        H = Random.Range(0.5f, 1f);
-        S = Random.Range(0.5f, 1f);
-        V = Random.Range(0.5f, 1f);
-    }
-
     private void Update()
     {
-        time += Time.deltaTime * 10;
-        gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.HSVToRGB((time / 100f) % H, S, V));
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene("Menu");
